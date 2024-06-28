0a64000bce24c9a2cf6a7cd08029c16758598393
KirillMelanich <kirillmelanich@gmail.com>
2024-06-28 15:23:21 +0200

    Part 2

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
 import telebot
+from telebot import types
 import webbrowser
 
 # connect script to bot with help of token
 bot =telebot.TeleBot('6382654428:AAEYIWnStZ1u3kFtw-J1LTjtvYwLmcE_mNw')
+
+def on_click(message):
+    if message.text == "Go to the site":
+        bot.send_message(message.chat.id, "Website is open")
+    elif message.text == "Delete photo":
+        bot.send_message(message.chat.id, "Deleted, sucker!")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    #creation of the buttons
+    markup = types.ReplyKeyboardMarkup()
+    btn1 =  types.KeyboardButton("Go to the site")
+    markup.row(btn1)
+    btn2 = types.KeyboardButton("Delete photo")
+    btn3 = types.KeyboardButton("Edit text")
+    markup.row(btn2, btn3)
+    file = open('./photo.JPEG', "rb")
+    bot.send_photo(message.chat.id, file, reply_markup=markup)
+    #bot.send_message(message.chat.id, "Hello, motherfucker!", reply_markup=markup)
+
+    bot.register_next_step_handler(message, on_click)
+
+
+"""
+Sends reply's for different files downloads
+"""
+@bot.message_handler(content_types=["photo", 'audio'])
+def get_photo(message):
+    #creation of the buttons
+    markup = types.InlineKeyboardMarkup()
+    btn1 =  types.InlineKeyboardButton("Go to the site", url="https://youtube.com")
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton("Delete photo", callback_data="delete")
+    btn3 = types.InlineKeyboardButton("Edit text", callback_data="edit")
+    markup.row(btn2, btn3)
+
+    bot.reply_to(message, "What a beautiful photo!", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == "delete":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+    elif callback.data == "edit":
+        bot.edit_message_text("Edit text",callback.message.chat.id, callback.message.message_id)
+
 
 """
 For sending user to website 
